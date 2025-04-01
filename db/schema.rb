2beb8ebbd7e9a978490fe968e84959f7fab1fba4
Cameron Latham <cameron.latham@solirius.com>
2025-04-01 10:44:30 +0100

    Dropped entries table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_31_134849) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_01_094135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "entries", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "comment"
-  end
 end
