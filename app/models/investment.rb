e562e44d3f0ec5972547ddc798df643890e1a950
Cameron Latham <cameron.latham@solirius.com>
2025-04-03 13:38:04 +0100

    put basic chart dependencies in and displayed the blank data in compare investments page

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -4,11 +4,15 @@ class Investment < ApplicationRecord
   validates :initial_deposit, :monthly_contribution, :duration, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :rate, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
+  # Method takes cur_amount, for each month in each year:
+  # it adds monthly contribution to current amount + interest generated
   def calculate_growth
     Money.default_currency = "GBP"
     current_amount = Money.new(initial_deposit * 100) # Convert to pennies
     (1..duration).each do |year|
-      current_amount += current_amount * (rate / 100)
+      (1..12).each do |month|
+        current_amount += current_amount * (rate / 100) + monthly_contribution
+      end
       # Use current_amount.format to display the value
     end
   end
